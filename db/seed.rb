e9fbbe50c7074488a92c2de1d0b1e7361c00fa1a
bbeaird <apprentice@dbc18.local>
2013-11-08 21:03:54 -0800

    Fixed seed.rb file to distribute the photos evenly AROUND a given lat, long

--- db/seed.rb
+++ db/seed.rb
@@ -1,14 +1,35 @@
 require './app'
 
+
+def location_helper(coordinate_type)
+  chance = rand(2)+1
+  case coordinate_type
+  when :latitude
+    if chance == 2
+      (37.768815 * (rand/1000 + 1))
+    elsif chance == 1
+      (37.768815 * (-rand/1000 + 1))
+    end
+  when :longitude
+      if chance == 2
+      (-122.439736 * (rand/2000 + 1))
+    elsif chance == 1
+      (-122.439736 * (-rand/2000 + 1))
+    end
+  end
+end
+
 100.times do |i|
-  Photo.create(
-  :insta_id => i,
-  :latitude => (37.768815 * (rand/1000 + 1)),
-  :longitude => (-122.439736 * (rand/1000 + 1)),
-  :location_name => Faker::Address.street_name,
-  :created_time => 1383288850, 
-  :like_count => rand(20), 
-  :link => 'http://instagram.com/p/gKaFkat0Tj/' , 
-  :thumbnail_url => "http://origincache-prn.fbcdn.net/1173089_566382760098534_103778226_s.jpg",
-  :caption => Faker::Lorem.words.map { |word| "#"+word }.join(" ") )
+  photo_attributes = {
+    :insta_id => i,
+    :latitude => location_helper(:latitude),
+    :longitude => location_helper(:longitude),
+    :location_name => Faker::Address.street_name,
+    :created_time => 1383288850, 
+    :like_count => rand(20), 
+    :link => 'http://instagram.com/p/gKaFkat0Tj/' , 
+    :thumbnail_url => "http://origincache-prn.fbcdn.net/1173089_566382760098534_103778226_s.jpg",
+    :caption => Faker::Lorem.words.map { |word| "#"+word }.join(" ")
+  }
+  Photo.create(photo_attributes)
 end
--- photo.rb
+++ photo.rb
@@ -1,3 +1,4 @@
 class Photo < ActiveRecord::Base
 
+
 end
