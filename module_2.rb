679e0fe5f49ee882ce23b7f172d14f8fab8c2a09
bbeaird <apprentice@dbc18.local>
2013-11-09 17:09:52 -0800

    cleaned up files

--- module_2.rb
+++
@@ -1,7 +0,0 @@
-def instagram_api_call(array_of_time_windows)
-  array_of_time_windows.each do |time_window|
-    array_of_media_JSONs = Instagram.media_search("37.768815","-122.439736", {distance: 5000, max_timestamp: time_window[1], min_timestamp: time_window[0]})
-    sleep(5)
-    return array_of_media_JSONs
-  end
-end
--- module_4.rb
+++
@@ -1,9 +0,0 @@
-def write_to_DB(photo_attributes)
-  Photo.create(photo_attributes)
-end
-
-def write_to_DB(photo_attributes_array)
-  photo_attributes_array.each do |photo|
-    Photo.create(photo)
-  end
-end
