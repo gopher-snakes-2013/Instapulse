ea129ef6da7dddc46289e5fc883c4109e405c13b
salarkhan <salar.k3@gmail.com>
2013-11-10 18:18:32 -0800

    Photo.create_tuples(start, end, interval) works

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,10 +6,33 @@ class Photo < ActiveRecord::Base
 	def self.grab_media_info(media)
 		output = []
 		media.each do |photo|
-			lat = photo.latitude
-			long = photo.longitude
-			thumbnail_url = photo.thumbnail_url
-			output << [lat,long, thumbnail_url]
+			photo_object = {}
+			photo_object["lat"] = photo.latitude
+			photo_object["long"] = photo.longitude
+			photo_object["thumbnail_url"] = photo.thumbnail_url
+			output << photo_object
+		end
+		output
+	end
+
+	def self.create_tuples(start_time, end_time, interval)
+		time_keeper = start_time
+		batch_time = interval / 2
+		iterate = true
+		output = []
+		until iterate == false
+			tuple = []
+			batch_A_complete_objects = Photo.where(created_time: (time_keeper)..(time_keeper + batch_time))
+			batch_A_digested = Photo.grab_media_info(batch_A_complete_objects)
+			tuple << batch_A_digested
+			batch_B_complete_objects = Photo.where(created_time: (time_keeper + batch_time)..(time_keeper + interval))
+			batch_B_digested = Photo.grab_media_info(batch_B_complete_objects)
+			tuple << batch_B_digested
+			output << tuple
+			time_keeper += batch_time
+			if time_keeper >= end_time
+				iterate = false
+			end
 		end
 		output
 	end
