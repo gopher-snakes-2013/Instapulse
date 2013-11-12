54e286d61009c6fafb5e4391284b97ae8c1408ac
salarkhan <salar.k3@gmail.com>
2013-11-11 16:23:26 -0800

    form_interpreter module has full test coverage

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ end
 
 group :development, :test do
   gem 'rspec-rails'
+  gem 'factory_girl_rails', "~> 4.0"
 end
 
 # Gems used only for assets and not required
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -2,32 +2,25 @@ require 'spec_helper'
 
 
 describe Photo do
-  let(:photo) {
-    photo = Photo.new
-    photo.insta_id = "xfsdfxfdsf34"
-    photo.latitude = 2222.0
-    photo.longitude = 1111.0
-    photo.thumbnail_url = "imapicture.jpg"
-    photo.created_time = 20
-    photo
-   }
+  let(:photo_template) { build(:photo) }
+  let(:photo) { create(:photo) }
 
   it "should save with valid attributes" do
     expect {
-      photo.save
+      photo
     }.to change{Photo.count}.from(0).to(1)
   end
 
   it "should not save without an insta_id" do
     expect{
-    	photo.insta_id = ""
-    	photo.save
+    	photo_template.insta_id = ""
+    	photo_template.save
     }.to change{Photo.count}.by(0)
   end
 
 
   it "should have a unique insta_id" do
-    photo.save
+    photo
     expect {
       Photo.create(insta_id: "xfsdfxfdsf34")
     }.to_not change{Photo.count}
@@ -35,7 +28,7 @@ describe Photo do
 
   context "grab_media_info" do
     it "should return an array of objects from db with latitude, longitude, and thumbnail_url" do
-    	photo.save
+    	photo
       photo_from_db = Photo.where(created_time: 20)
 
     	expect(Photo.grab_media_info(photo_from_db)[0]).to include("latitude" => 2222.0, "longitude" => 1111.0, "thumbnail_url" => "imapicture.jpg") 
@@ -44,7 +37,7 @@ describe Photo do
 
   context "create_tuples" do
     it "should return an array of tuples of objects" do
-      photo.save
+      photo
       expect(Photo.create_tuples({:start_time => 0, :end_time => 30, :interval => 30})[0][1][0]).to include("latitude" => 2222.0)
     end
   end
