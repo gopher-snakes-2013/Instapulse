75ec559e0c44541f7ab75b98c39dce252bdcde62
ryanhedges <ryanhedges@gmail.com>
2013-11-12 09:22:38 -0800

    can't delete layers

--- app/assets/javascripts/mapBoxApp.js
+++ app/assets/javascripts/mapBoxApp.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   MapBuilder.map = MapBuilder.createMap()
-  MapBuilder.spittleLayer = L.mapbox.markerLayer().addTo(MapBuilder.map)
   TimeSelector.initialize()
 })
 
@@ -60,7 +59,7 @@ Converter = {
           icon: {
             iconUrl: "http://imgur.com/hZE9VrA.png",
             iconSize: [6,6],
-            iconAnchor: [10,10]
+            iconAnchor: [3,6]
           }
         }
       }
@@ -87,12 +86,6 @@ MapBuilder = {
     }, 1000)
   },
 
-
-  createGeoJSONLayer: function(geoJSON){
-    MapBuilder.map.markerLayer.setGeoJSON(geoJSON)
-    debugger
-  },
-
   addMarkersToLayer: function(photoObjects){
     var counter = 0
     var tupleInterval = setInterval(function(){
@@ -111,6 +104,16 @@ MapBuilder = {
     }, 500)
   },
 
+  createGeoJSONLayer: function(geoJSON){
+    MapBuilder.map.markerLayer.eachLayer(  
+      function(l) { MapBuilder.map.markerLayer.removeLayer(l); }
+    );
+    MapBuilder.map.markerLayer.setGeoJSON(geoJSON)
+    console.log(MapBuilder.spittle)
+    debugger
+    MapBuilder.spittle = L.mapbox.markerLayer().addTo(MapBuilder.map)
+  },
+
   decideWhetherToPost: function(photoObjectArray){
       var counter = 0
       var objectInterval = setInterval(function(){
@@ -118,8 +121,7 @@ MapBuilder = {
           clearInterval(objectInterval)
         }
         else {
-          // MapBuilder.spittleLayer = L.mapbox.markerLayer().addTo(MapBuilder.map)
-          MapBuilder.spittleLayer = L.mapbox.markerLayer(photoObjectArray[counter]).addTo(MapBuilder.map)
+          L.mapbox.markerLayer(photoObjectArray[counter]).addTo(MapBuilder.spittle)
           counter++
         }
       }, 50)
