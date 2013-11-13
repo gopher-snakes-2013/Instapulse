73d7681ed18537e1ab8543d8a4004ad587eda6f8
salarkhan <salar.k3@gmail.com>
2013-11-13 11:06:48 -0800

    Changed production to no longer precompile assets.  Bugfix: heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Instapulse::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.assets.initialize_on_precompile = false
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
