c563a57df5df79c66aac2fa412a9ce0aa88b55ca
supertopher <asimplechris@gmail.com>
2013-11-13 11:31:00 -0800

    Fucking css

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,6 @@ module Instapulse
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
