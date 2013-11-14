8f6d8aa8423d33c011cc264819feeab960af21df
salarkhan <salar.k3@gmail.com>
2013-11-13 23:42:19 -0800

    ANOTHER FEATURE IS CRUSHED. DIfferent icon based on like count

--- app/assets/javascripts/layer_helpers.js
+++ app/assets/javascripts/layer_helpers.js
@@ -5,12 +5,17 @@ LayerHelpers = {
 
   changeMarkerIcon: function(marker) {
     if(marker.feature){
-      marker.setIcon(L.icon(marker.feature.properties.icon));
+
+      if(marker.feature.properties.like_count > 30) {
+        marker.setIcon(L.icon(marker.feature.properties.pink_icon));
+      } else {
+        marker.setIcon(L.icon(marker.feature.properties.blue_icon));
+      }
       var popupContent = '<img class ="pop-up" src=' + marker.feature.properties.image + '>'
       marker.bindPopup(popupContent, {
         closeButton: true
       });
-    }    
+    }
   },
 
   markerAddRemove: function(arrayOfGeoJSONs){
--- app/assets/javascripts/photos_to_geo_jsons.js
+++ app/assets/javascripts/photos_to_geo_jsons.js
@@ -18,10 +18,17 @@ PhotosToGeoJSONs = {
       properties: {
         image: photoJSON.thumbnail_url,
         link_url: photoJSON.link_url,
+        like_count: photoJSON.like_count,
         caption: photoJSON.photo_caption,
         created_time: photoJSON.created_time,
-        icon: {
+        blue_icon: {
           iconUrl: "http://imgur.com/hZE9VrA.png",
+          iconSize: [6,6],    // 6x6 pixels
+          iconAnchor: [3,6],  // 3x6 pixels
+          className: "ig-dot"
+        },
+        pink_icon: {
+          iconUrl: "http://3.bp.blogspot.com/-EWiwSDU-oVs/T6O7xBc9L6I/AAAAAAAABKA/htr8B3sVeFc/s1600/favicon.png",
           iconSize: [6,6],    // 6x6 pixels
           iconAnchor: [3,6],  // 3x6 pixels
           className: "ig-dot"
