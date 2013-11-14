26bb20b1c52cc1a024cacc7cae6ec97ea5788eb3
ryanhedges <ryanhedges@gmail.com>
2013-11-13 17:52:59 -0800

    Added case switch statement and html to hndle event selection

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -18,6 +18,34 @@ LayerHelpers = {
 }
 
 FormHelpers = {
+
+  getSelectedEventTimeStamp: function() {
+    var event_name = $('#event')[0].options[$('#event')[0].selectedIndex].value
+    var unixSelectedDate;
+    switch (event_name)
+    {
+      case 'Halloween':
+        unixSelectedDate = 1383202800;
+        break;
+      case 'Bay to Breakers':
+        unixSelectedDate = 1368946800;
+        break;
+      case 'SF Giants':
+        unixSelectedDate = 1351407600;
+        break;
+      case 'Folsom Street Fair':
+        unixSelectedDate = 1380438000;
+        break;
+      case 'Hottest day of the year':
+        unixSelectedDate = 1383202800;
+        break;
+      case 'Coldest day of the year':
+        unixSelectedDate = 1383202800;
+        break;
+    }
+    return unixSelectedDate
+  },
+
   getSelectedSpeed: function() {
     var speed = $('#speed')[0].options[$('#speed')[0].selectedIndex].value
     return speed
@@ -34,7 +62,7 @@ FormHelpers = {
   },
 
   mapStartTime: function() {
-    var unixSelectedDate = 1383202800
+    var unixSelectedDate = FormHelpers.getSelectedEventTimeStamp()
     var unixStartHour = 36 * FormHelpers.getStartTime()
     return unixSelectedDate + unixStartHour
   },
